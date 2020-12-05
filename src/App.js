460dab37abb349214e72f3386e835eba4b4804d8
Zulai Wang <wzl574402791@sjtu.edu.cn>
2020-12-05 17:52:23 +0800

    add patient page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import PatientPage from './page/PatientPage'
+import DoctorPage from './page/DoctorPage'
+import AdminPage from './page/AdminPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Switch>
+            <Route exact path={'/'} component={PatientPage}/>
+            <Route path={'/doctor'} component={DoctorPage} />
+            <Route path={'/admin'} component={AdminPage} />
+          </Switch>
+      </Router>
   );
 }
 
---
+++ src/page/PatientPage.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid/index'
+import { Layout, List, Menu, Button, Row, Table } from "antd";
+import { Link } from "react-router-dom";
+
+const { Content, Sider } = Layout;
+
+// const data = [
+//     'Racing car sprays burning fuel into crowd.',
+//     'Japanese princess to wed commoner.',
+//     'Australian walks 100km after outback crash.',
+//     'Man charged over missing wedding girl.',
+//     'Los Angeles battles huge wildfires.',
+//     "123"
+// ]
+
+const columns_register = [
+    {
+        title: 'Number',
+        dataIndex: 'number',
+    },
+    {
+        title: 'Admission',
+        dataIndex: 'admission',
+    },
+    {
+        title: 'Checklist Detail',
+        dataIndex: 'checklist',
+    }
+];
+
+const columns_prescription = [
+    {
+        title: 'Number',
+        dataIndex: 'number',
+    },
+    {
+        title: 'Medicine Receipt',
+        dataIndex: 'receipt',
+    },
+    {
+        title: 'Payment',
+        dataIndex: 'payment',
+    },
+];
+
+const data_register = [
+    {
+        key: '1',
+        number: '1',
+        admission: 'Not checked',
+        checklist: <Button>checklist</Button>,
+    },
+];
+
+const data_prescription = [
+    {
+        key: '1',
+        number: '1',
+        receipt: <Button>receipt</Button>,
+        payment: <Button>payment</Button>,
+    },
+];
+
+class PatientPage extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+        <Layout style={{minHeight: '100vh'}}>
+            <Sider>
+                <Menu>
+                    <Menu.Item key="1">
+                        <Row justify="center"><Button type="primary">Register</Button></Row>
+                    </Menu.Item>
+                    <Menu.Divider style={{margin:20}}/>
+                    <Menu.Item key="2">
+                        <Row justify="center"><Link to={'/'}><span>Register Info</span></Link></Row>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Row justify="center"><Link to={'/'}><span>Prescription Info</span></Link></Row>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Content style={{margin: '16px'}}>
+                    <div id={"content"}>
+                        <Grid container direction={"column"} >
+                            <Grid container direction={"row"} >
+                                <Grid item xs={2} />
+                                <Grid item xs={8} >
+                                    <br/><br/>
+                                    <h1>Register Information</h1>
+                                    <Table columns={columns_register} dataSource={data_register} />
+                                    <br/><br/>
+                                    <h1>Prescription Information</h1>
+                                    <Table columns={columns_prescription} dataSource={data_prescription} />
+                                </Grid>
+                                <Grid item xs={2} />
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
+        )
+    }
+}
+
+export default PatientPage;
